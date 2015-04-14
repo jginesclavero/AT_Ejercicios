f774aac5376f9dd44c6578756928fac3854e1e33
Jonathan Gines <jonathangines@hotmail.com>
2015-04-14 14:34:49 +0200

    Nominatim terminado

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -3,11 +3,11 @@ function readComand(s){
 	switch (s) {
 		case "1":
 			var input = document.getElementById("display");
-         	input.innerHTML = "1";
+         	input.innerHTML = input.innerHTML + "1";
 			break;
 		case "0":
 			var input = document.getElementById("display");
-         	input.innerHTML = "0";
+         	input.innerHTML = input.innerHTML + "0";
 			break;
 		case "+":
 			input = document.getElementById("display");
@@ -19,6 +19,16 @@ function readComand(s){
 			input = document.getElementById("display");
 			op1 = document.getElementById("dataBase");
          	input.innerHTML = binaryAdd(op1.innerHTML,input.innerHTML);
+			break;
+		case "clear":
+			input = document.getElementById("display");
+         	input.innerHTML = "";
+         	document.getElementById("dataBase").innerHTML = "";
+			break;
+		case "<":
+			input = document.getElementById("display");
+			s = input.innerHTML;
+         	input.innerHTML = s.slice(0,-1);
 			break;
 		default:
 			var res = document.getElementById("display");
--- leaflet/nominatim/nominatim.js
+++ leaflet/nominatim/nominatim.js
@@ -1,5 +1,6 @@
+var map;
 jQuery(document).ready(function(){
-	var map = L.map('map');
+	map = L.map('map');
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {  // http://{s}.tile.osm.org/{z}/{x}/{y}.png
     	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
@@ -16,3 +17,37 @@ jQuery(document).ready(function(){
 	map.on('locationfound', onLocationFound);
 
 });
+
+function addr_search() {
+  	var inp = document.getElementById("addr");
+  	$.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + inp.value, function(data) {
+  		var items = [];
+		$.each(data, function(key, val) {
+  			items.push(
+    		"<li><a href='#' onclick='chooseAddr(" +
+    		val.lat + ", " + val.lon + ");return false;'>" + val.display_name +
+    		'</a></li>'
+ 		 	);
+ 		 });
+  		$('#results').empty();
+    	if (items.length != 0) {
+    		$('<p>', { html: "Search results:" }).appendTo('#results');
+    		$('<ul/>', {
+       		'class': 'my-new-list',
+       		html: items.join('')
+    		}).appendTo('#results');
+    	} else {
+    		$('<p>', { html: "No results found" }).appendTo('#results');
+   		}
+  	});
+}
+
+function chooseAddr(lat, lng, type) {
+	var location = new L.LatLng(lat, lng);
+	map.panTo(location);
+	if (type == 'city' || type == 'administrative') {
+		map.setZoom(11);
+	} else {
+		map.setZoom(13);
+	}
+}
